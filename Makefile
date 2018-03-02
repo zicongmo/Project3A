dfd1a18d99971385c4cce68cbbe6cfae201b607f
Joseph Mo <josephmo1594@ucla.edu>
2018-03-02 13:35:49 -0800

    Change to C

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 default:
-	g++ -Wall -Wextra lab3a.cpp -o lab3a 
+	gcc -Wall -Wextra lab3a.c -o lab3a 
 clean:
 	rm -f lab3a lab3a-804654167.tar.gz
 dist:
--- lab3a.c
+++ lab3a.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <iostream> 
 #include <stdlib.h>
-#include <vector>
-#include <string>
-using namespace std;
 
 int main(int argc, char** argv){
 	if(argc != 2){
