1ead6b23a82409b381009ce621e7ec30c905c3f4
Joseph Mo <josephmo1594@ucla.edu>
2018-03-03 18:16:28 -0800

    Getting super

--- lab3a.c
+++ lab3a.c
@@ -6,6 +6,12 @@ EMAIL: josephmo1594@ucla.edu, byang77@ucla.edu
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>	
+#include "ext2_fs.h"
 
 int main(int argc, char** argv){
 	if(argc != 2){
@@ -13,5 +19,25 @@ int main(int argc, char** argv){
 		exit(1);
 	}
 
+	int fd = open(argv[1], O_RDONLY);
+	if(fd < 0){
+		fprintf(stderr, "Error: %s\n", strerror(errno));
+		exit(1);
+	}
+
+	/* Get the superblock */
+	struct ext2_super_block super;
+	lseek(fd, 1024, SEEK_SET); /* Put file offeset at 1024 */
+	int r = read(fd, &super, sizeof(super));
+	if(r < 0){
+		fprintf(stderr, "Error: %s\n", strerror(errno));
+		exit(1);
+	}
+	if(super.s_magic != EXT2_SUPER_MAGIC){
+		fprintf(stderr, "Error: Specified file is not EXT2\n");
+		exit(1);
+	}
+
+	printf("Num inodes: %u\n", super.s_inodes_count);
 	exit(0);
 }
